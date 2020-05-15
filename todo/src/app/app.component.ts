35577b13325c7d2fc621b8a72922a53f0653b40c
rohitpjpti18 <rohitpjpti18@gmail.com>
2020-05-15 20:25:41 +0530

    add todo

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Model, TodoItem } from "./model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    model = new Model();
+    title = 'todo';
+
+    getName() {
+        return this.model.user;
+    }
+
+    getTodoItems() {
+        return this.model.items.filter(item => !item.done);
+    }
+
+    addItem(newItem){
+        if(newItem != ""){
+            this.model.items.push(new TodoItem(newItem, false));
+        }
+    }
+}
